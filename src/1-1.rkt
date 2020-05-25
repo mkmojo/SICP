b5c7aca90dcf3a1de36c2d3addc477b479256541
Michael Qiu <qiuqiyuan@users.noreply.github.com>
2020-05-25 14:23:29 -0400

    use racket

--- src/1-1.rkt
+++ src/1-1.rkt
@@ -1,9 +1,12 @@
-#lang sicp
-(inc 42)
-486
+#lang racket
 
+;; This is a comment
 (+  (* 3 
         (+ (* 2 4) 
             (+ 3 5)))
     (+ (- 10 7) 
         6))
+
+;; 1.1.2
+(define size 2)
+(display size)
