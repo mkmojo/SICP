920ade0f0c91ddce15a0dc29868f50fc154bcf9e
Michael Qiu <qiuqiyuan@users.noreply.github.com>
2020-05-25 14:26:44 -0400

    update

--- src/1-1.rkt
+++ src/1-1.rkt
@@ -1,12 +1,12 @@
 #lang racket
 
-;; This is a comment
+;; 1.1.1 Expressions
 (+  (* 3 
         (+ (* 2 4) 
             (+ 3 5)))
     (+ (- 10 7) 
         6))
 
-;; 1.1.2
+;; 1.1.2 Naming and the Environment
 (define size 2)
-(display size)
+(print size)
